98f686cb8051349a22e596e01b9c0e095eb54aab
NeoPhetoane <phetoaneneo4@gmail.com>
2024-06-09 00:15:51 +0200

    Enum Types: Constants created to decide whether a reviewer is a loyalty customer or not, the enums are added to their own file and the star display function edited to take in LoyaltyUser instead of a boolean.

--- utils.ts
+++ utils.ts
@@ -1,11 +1,11 @@
 const returningUserDisplay = document.querySelector("#returning-user");
 const userNameDisplay = document.querySelector("#user");
 const reviewTotalDisplay = document.querySelector("#reviews");
-
+import { LoyaltyUser } from './enums'
 //function to show total number of reviews and only accepts numbers
 
-function showReviewTotal(value: number, reviewer: string, isLoyalty: boolean) {
-    const iconDisplay = isLoyalty ? "⭐" : "";
+function showReviewTotal(value: number, reviewer: string, isLoyalty:LoyaltyUser) {
+    const iconDisplay = LoyaltyUser.GOLD_USER? "⭐" : "";
     reviewTotalDisplay.innerHTML =
       "review total" +
       value.toString() +
