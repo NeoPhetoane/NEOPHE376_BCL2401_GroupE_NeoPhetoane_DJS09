ef5c06e4772eefdb8d6f0cd58045eb7158336424
NeoPhetoane <phetoaneneo4@gmail.com>
2024-06-09 01:04:19 +0200

    Function Type: Return type added to makeMultiple and then used in showReviewTotal

--- utils.ts
+++ utils.ts
@@ -1,26 +1,47 @@
 const returningUserDisplay = document.querySelector("#returning-user");
 const userNameDisplay = document.querySelector("#user");
 const reviewTotalDisplay = document.querySelector("#reviews");
-import { LoyaltyUser } from './enums'
+import { LoyaltyUser } from "./enums";
+
 //function to show total number of reviews and only accepts numbers
+function showReviewTotal(
+  value: number,
+  reviewer: string,
+  isLoyalty: LoyaltyUser
+) {
+  const iconDisplay = LoyaltyUser.GOLD_USER ? "⭐" : "";
+  reviewTotalDisplay.innerHTML =
+    value.toString() +
+    " Review" +
+    makeMultiple(value) +
+    "| last reviewed by " +
+    reviewer +
+    " " +
+    iconDisplay;
+}
 
-function showReviewTotal(value: number, reviewer: string, isLoyalty:LoyaltyUser) {
-    const iconDisplay = LoyaltyUser.GOLD_USER? "⭐" : "";
-    reviewTotalDisplay.innerHTML =
-      "review total" +
-      value.toString() +
-      "| last reviewed by " +
-      reviewer +
-      " " +
-      iconDisplay;
+function populateUser(isReturning: boolean, userName: string) {
+  if (isReturning) {
+    returningUserDisplay.innerHTML = "back";
   }
+  userNameDisplay.innerHTML = userName;
+}
 
-  function populateUser(isReturning: boolean, userName: string) {
-    if (isReturning) {
-      returningUserDisplay.innerHTML = "back";
-    }
-    userNameDisplay.innerHTML = userName;
+//Function to display the price
+export function showDetails(
+  value: boolean | Permissions,
+  element: HTMLDivElement,
+  price: number
+) {
+  if (value) {
+    const priceDisplay = document.createElement("div");
+    priceDisplay.innerHTML = price.toString() + "/night";
+    element.appendChild(priceDisplay);
   }
-  
+}
 
-  
+export function makeMultiple(value: number): string {
+  if (value > 1 || value == 0) {
+    return "s";
+  } else return "";
+}
