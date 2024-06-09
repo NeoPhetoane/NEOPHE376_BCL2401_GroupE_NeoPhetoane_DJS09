8a1c64b5d98998720aa217b03f5b90ca9f3edbc0
NeoPhetoane <phetoaneneo4@gmail.com>
2024-06-09 20:20:14 +0200

    New Property: Property added, its price and country was updated in the types and styling added for image size

--- types.ts
+++ types.ts
@@ -1,3 +1,3 @@
 //Constant literal type that ensure the data entered under it is exactly the data specified.
-export type Price = 45 | 30 | 25
-export type Country = 'Colombia' | 'Poland' | 'United Kingdom'
+export type Price = 45 | 30 | 25 | 35
+export type Country = 'Colombia' | 'Poland' | 'United Kingdom' | 'Malaysia'
